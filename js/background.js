5535e1f3e742143533a6b2b6fab234714e4308f2
DABU <daburutti@gmail.com>
2020-09-30 10:22:57 +0900

    I duuno about this but I keep going...

--- js/background.js
+++ js/background.js
@@ -1,18 +1,25 @@
 let dot = new Array();
-var maxHeight = window.innerHeight;
+//var maxWidth = window.innerWidth;
+var sap = 20;
+var boxWidth = window.innerWidth / sap;
 
 function setup(){
     createCanvas(window.innerWidth,window.innerHeight);
     background(color('#ffc533'));
-    for(var i = 0; i < 100 ; i++){
-        dot_instance = new Dot(Math.random()*window.innerWidth,Math.random()*window.innerHeight,1,20);
-        dot.push(dot_instance);
+    for(var i = 0; i < sap ; i++){
+        for(var j = 0; j < window.innerHeight/boxWidth; j++){
+            dot_instance = new Dot(i * boxWidth,
+                j * boxWidth,Math.random());
+                dot.push(dot_instance);
+        }
     }
     maxHeight = window.innerHeight;
+    boxWidth = window.innerWidth / sap;
 }
 
 function draw(){
-    background(color('#ffc53305'));
+    //background(color('#ffc533'));
+    background(color('#000000'));
     dot.forEach(element => {
         element.move();
     });
@@ -26,19 +33,19 @@ class Dot{
     constructor(x,y,speed,size){
         this.x = x;
         this.y = y;
-        this.speed = speed;
-        this.size = size;
+        this.speed = speed+0.1;
+        this.size = (speed) * 10+5;
+        this.color = speed * 255;
     }
 
     move(){
-        this.y += this.speed;
-        noStroke();
-        fill(255,255,255,40);
-        ellipse(this.x,this.y,this.size,this.size);
+        //this.x += this.speed;
+        stroke(color("#5797ff"));
+        //fill(sin()*255);
+        fill(255);
+        rect(this.x,this.y,boxWidth,boxWidth);
         //text(frameCount/millis()*1000,this.x+10,this.y);
 
-        if(this.y - this.size >= maxHeight){
-            this.y = -this.size;
-        }
+
     }
 }
--- js/dist/background.dev.js
+++ js/dist/background.dev.js
@@ -6,23 +6,29 @@ function _defineProperties(target, props) { for (var i = 0; i < props.length; i+
 
 function _createClass(Constructor, protoProps, staticProps) { if (protoProps) _defineProperties(Constructor.prototype, protoProps); if (staticProps) _defineProperties(Constructor, staticProps); return Constructor; }
 
-var dot = new Array();
-var maxHeight = window.innerHeight;
+var dot = new Array(); //var maxWidth = window.innerWidth;
+
+var sap = 20;
+var boxWidth = window.innerWidth / sap;
 
 function setup() {
   createCanvas(window.innerWidth, window.innerHeight);
   background(color('#ffc533'));
 
-  for (var i = 0; i < 100; i++) {
-    dot_instance = new Dot(Math.random() * window.innerWidth, Math.random() * window.innerHeight, 1, 20);
-    dot.push(dot_instance);
+  for (var i = 0; i < sap; i++) {
+    for (var j = 0; j < window.innerHeight / boxWidth; j++) {
+      dot_instance = new Dot(i * boxWidth, j * boxWidth, Math.random());
+      dot.push(dot_instance);
+    }
   }
 
   maxHeight = window.innerHeight;
+  boxWidth = window.innerWidth / sap;
 }
 
 function draw() {
-  background(color('#ffc53305'));
+  //background(color('#ffc533'));
+  background(color('#000000'));
   dot.forEach(function (element) {
     element.move();
   });
@@ -40,21 +46,19 @@ function () {
 
     this.x = x;
     this.y = y;
-    this.speed = speed;
-    this.size = size;
+    this.speed = speed + 0.1;
+    this.size = speed * 10 + 5;
+    this.color = speed * 255;
   }
 
   _createClass(Dot, [{
     key: "move",
     value: function move() {
-      this.y += this.speed;
-      noStroke();
-      fill(255, 255, 255, 40);
-      ellipse(this.x, this.y, this.size, this.size); //text(frameCount/millis()*1000,this.x+10,this.y);
+      //this.x += this.speed;
+      stroke(color("#5797ff")); //fill(sin()*255);
 
-      if (this.y - this.size >= maxHeight) {
-        this.y = -this.size;
-      }
+      fill(255);
+      rect(this.x, this.y, boxWidth, boxWidth); //text(frameCount/millis()*1000,this.x+10,this.y);
     }
   }]);
 
