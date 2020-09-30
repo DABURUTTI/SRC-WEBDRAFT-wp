32232354853483d0c198d8f45bc8e26fa3869cfb
DABU <daburutti@gmail.com>
2020-09-30 21:23:55 +0900

    ok. I still don't get it but I felt something....

--- js/dist/background.dev.js
+++ js/dist/background.dev.js
@@ -8,11 +8,10 @@ function _createClass(Constructor, protoProps, staticProps) { if (protoProps) _d
 
 var dot = new Array(); //var maxWidth = window.innerWidth;
 
-var sap = 30;
+var sap = 5;
 var boxWidth = window.innerWidth / sap;
 var logo;
-var pointer;
-var pointer2;
+var pointers = new Array();
 
 function setup() {
   logo = loadImage("../img/logo_w.svg");
@@ -21,37 +20,29 @@ function setup() {
 
   for (var i = 0; i < sap; i++) {
     for (var j = 0; j < window.innerHeight / boxWidth; j++) {
-      dot_instance = new Dot(i * boxWidth, j * boxWidth, Math.random());
-      dot.push(dot_instance);
+      pointer_instance = new Pointer(Math.random() * windowWidth, Math.random() * windowHeight, 200);
+      pointers.push(pointer_instance);
     }
   }
 
-  pointer = new Pointer(200, 200, 120);
-  pointer2 = new Pointer(200, 200, 120);
   maxHeight = window.innerHeight;
   boxWidth = window.innerWidth / sap;
 }
 
 function draw() {
-  background(color('#ffbc1f20')); //background(color('#000000'));
-  // dot.forEach(element => {
-  //     element.move();
-  // });
-
+  background(color('#ffffff20'));
+  pointers.forEach(function (element) {
+    element.move();
+  });
   noStroke();
   fill(0);
-  pointer.move();
-  pointer2.move();
   noStroke();
   rect(5, window.innerHeight - 25, 150, 20);
   fill(255);
   text("".concat(int(frameCount / millis() * 1000), " fps - mode : DEBUG"), 10, window.innerHeight - 10);
-
-  if (frameCount % 70 == 0) {
-    pointer.update(Math.random() * windowWidth, Math.random() * windowHeight, 100);
-    pointer2.update(Math.random() * windowWidth, Math.random() * windowHeight, 100);
-  }
-
+  pointers.forEach(function (element) {
+    element.update(Math.random() * windowWidth, Math.random() * windowHeight, 200);
+  });
   fill(0);
   image(logo, windowWidth / 2 - 300, windowHeight / 2 - 200, 600, 400);
 }
@@ -72,6 +63,7 @@ function () {
     this.destX = x;
     this.destY = y;
     this.nSize = size;
+    this.color = color(Math.random() * 255, Math.random() * 255, Math.random() * 255); //this.color = color(50,84,48);
   }
 
   _createClass(Pointer, [{
@@ -84,23 +76,23 @@ function () {
   }, {
     key: "move",
     value: function move() {
-      stroke(0, 0, 0, 20);
-      fill(0);
-      strokeWeight(3);
-      line(0, this.y, windowWidth, this.x);
-      line(0, this.x, windowWidth, this.y);
-      line(this.y, 0, this.x, windowHeight);
-      line(this.x, 0, this.y, windowHeight); //update position
+      stroke(this.color);
+      strokeWeight(53);
+      noFill();
+      ellipse(this.x, this.y, 500, 500); //update position
 
-      this.x = (this.destX - this.x) * 0.1 + this.x;
-      this.y = (this.destY - this.y) * 0.1 + this.y;
+      this.x = (this.destX - this.x) * 0.01 + this.x;
+      this.y = (this.destY - this.y) * 0.01 + this.y;
       this.size = (this.nSize - this.size) * 0.01 + this.size;
-      console.log(this.x);
     }
   }]);
 
   return Pointer;
 }();
+
+var colorDirector = function colorDirector() {
+  _classCallCheck(this, colorDirector);
+};
 
 var Dot =
 /*#__PURE__*/
@@ -119,9 +111,9 @@ function () {
     key: "move",
     value: function move() {
       //this.x += this.speed;
-      stroke(color("#5797ff")); //fill(sin()*255);
+      stroke(this.color); //fill(sin()*255);
 
-      fill(255);
+      fill(this.color);
       rect(this.x, this.y, boxWidth, boxWidth); //text(frameCount/millis()*1000,this.x+10,this.y);
     }
   }]);
